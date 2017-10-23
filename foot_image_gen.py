a297abfa7720bcbb200d3de9575b24c5824704ff
satojkovic <satojkovic@gmail.com>
2017-10-24 00:17:43 +0900

    Add sample script

---
+++ foot_image_gen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import tensorflow as tf
+import sys
+sys.path.append('../../tf-image-segmentation')
+sys.path.append('models/slim')
+
+from tf_image_segmentation.models.fcn_8s import FCN_8s
+from tf_image_segmentation.utils.inference import adapt_network_for_any_size_input
+
+if __name__ == '__main__':
+    number_of_classes = 21
+    image_filename = 'nacho_0.jpg'
+    image_filename_placeholder = tf.placeholder(tf.string)
+    feed_dict = {image_filename_placeholder: image_filename}
+    image_tensor = tf.read_file(image_filename_placeholder)
+    image_tensor = tf.image.decode_jpeg(image_tensor, channels=3)
+    image_batch_tensor = tf.expand_dims(image_tensor, axis=0)
+    FCN_8s = adapt_network_for_any_size_input(FCN_8s, 32)
