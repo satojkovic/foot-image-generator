b8b7c847d654dad179545b4f1ed0ef7f57bb439f
satojkovic <satojkovic@gmail.com>
2017-12-23 17:32:16 +0900

    Test

--- foot_image_gen.py
+++ foot_image_gen.py
@@ -4,8 +4,8 @@
 import numpy as np
 import tensorflow as tf
 import sys
-sys.path.append('../../tf-image-segmentation')
-sys.path.append('models/slim')
+sys.path.append('../../tf-image-segmentation/')
+sys.path.append('slim')
 
 from tf_image_segmentation.models.fcn_8s import FCN_8s
 from tf_image_segmentation.utils.inference import adapt_network_for_any_size_input
@@ -16,6 +16,12 @@ if __name__ == '__main__':
     image_filename_placeholder = tf.placeholder(tf.string)
     feed_dict = {image_filename_placeholder: image_filename}
     image_tensor = tf.read_file(image_filename_placeholder)
+    # tf.shape(image_tensor) = [H, W, CH]
     image_tensor = tf.image.decode_jpeg(image_tensor, channels=3)
+    # tf.shape(image_batch_tensor) = [1, H, W, CH]
     image_batch_tensor = tf.expand_dims(image_tensor, axis=0)
     FCN_8s = adapt_network_for_any_size_input(FCN_8s, 32)
+    pred, fcn_16s_variable_mapping = FCN_8s(
+        image_batch_tensor=image_batch_tensor,
+        number_of_classes=number_of_classes,
+        is_training=False)
